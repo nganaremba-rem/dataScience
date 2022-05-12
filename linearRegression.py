827cf75e9da991caf9f148a383f55bd2aebb7783
Remku <83505774+remku@users.noreply.github.com>
2022-05-12 10:49:24 +0530

    Create linearRegression.py

---
+++ linearRegression.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv("https://raw.githubusercontent.com/remku/fishData/main/Fish.csv")
+
+data.head(10)
+
+x = data.iloc[:,1]
+y = data.iloc[:,6]
+
+import matplotlib.pyplot as plt
+plt.scatter(x,y, color='r')
+
+from sklearn.linear_model import LinearRegression
+
+LM = LinearRegression()
+
+LM.fit(np.array(x).reshape(-1,1),y)
+
+
+y_hat = LM.predict(np.array(x).reshape(-1,1),y)
+
+
+plt.plot(y,color='r')

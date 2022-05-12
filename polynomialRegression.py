ad3817ae9d469fb616f525c67b3d11e384efa899
Remku <83505774+remku@users.noreply.github.com>
2022-05-12 11:42:08 +0530

    Update polynomialRegression.py

--- polynomialRegression.py
+++ polynomialRegression.py
@@ -1,22 +1,22 @@
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv("https://raw.githubcontentuser.com/remku/fishData/main/Fish.csv")
+data = pd.read_csv("https://raw.githubusercontent.com/remku/fishData/main/Fish.csv")
 
 x = data.iloc[:,1]
 y = data.iloc[:,6]
 
 from sklearn.preprocessing import PolynomialFeatures
 
-PF = PolynomialFeatures()
-y_poly = PF.fit_transform(np.array(y))
+PF = PolynomialFeatures(degree=4)
+x_poly = PF.fit_transform(np.array(x).reshape(-1,1))
 
 from sklearn.linear_model import LinearRegression
 
 LM = LinearRegression()
-LM.fit(y_poly)
+LM.fit(x_poly, y)
 
-y_hat_poly = LM.predict(y_poly)
+y_hat_poly = LM.predict(x_poly)
 
 plt.plot(y, color='r')
 plt.plot(y_hat_poly, color='g')
